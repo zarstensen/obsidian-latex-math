3dcb62941ab98a9a4f5a172b6054e3239bc4cd09
Simon Sørensen <61250140+zarstensen@users.noreply.github.com>
2025-09-05 18:19:28 +0200

    Rename 'pip.exe' to 'pip' for cross platform support (#146)

--- setup-dev-env.py
+++ setup-dev-env.py
@@ -12,5 +12,5 @@ builder.create(venv_dir)
 
 print("Installing venv dependencies")
 
-pip_executable = os.path.join(venv_dir, venv_bin_folder, "pip.exe")
+pip_executable = os.path.join(venv_dir, venv_bin_folder, "pip")
 subprocess.check_call([pip_executable, "install", "-r", "requirements.txt", "-r", "requirements-dev.txt"])
