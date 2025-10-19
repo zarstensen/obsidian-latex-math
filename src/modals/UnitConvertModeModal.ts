66239b60fd882f662a2f255c93c4ccb66dd679cc
alexanderNB <alexander@bjerking.dk>
2025-10-20 00:33:35 +0200

    added press enter to confirm for unit modal

--- src/modals/UnitConvertModeModal.ts
+++ src/modals/UnitConvertModeModal.ts
@@ -2,24 +2,24 @@ import { App, Modal, Setting } from "obsidian";
 
 // The UnitConvertModeModal provides a modal dialog to specify a list of units which should be converted to.
 export class UnitConvertModeModal extends Modal {
-    constructor(app: App)
-    {
+    constructor(app: App) {
         super(app);
 
         this.target_units_promise = new Promise((resolve, _reject) => {
             this.target_units_resolve = resolve;
         });
 
-        
+
         this.setTitle("Convert units");
-        
+
         // setup units input
         new Setting(this.contentEl)
             .setName("Target units")
             .addText(text => {
                 text.onChange((value) => {
                     this.target_units = value.split(' ').filter(unit => unit.trim() !== '');
-        });});
+                });
+            });
 
         // add convert button.
         new Setting(this.contentEl)
@@ -32,6 +32,11 @@ export class UnitConvertModeModal extends Modal {
                         this.target_units_resolve(this.target_units);
                     });
             });
+
+        this.scope.register(null, 'Enter', () => {
+            this.close();
+            this.target_units_resolve(this.target_units);
+        })
     }
 
     public getTargetUnits(): Promise<string[]> {
